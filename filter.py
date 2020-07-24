3932d7c7adc037c12d9c35c7fabe2432fc1d1459
Study-is-happy <zhang.zhiyo@husky.neu.edu>
2020-07-24 10:38:11 -0400

    t

--- filter.py
+++ filter.py
@@ -7,7 +7,7 @@ import util
 
 # TODO: Set the dirs
 
-dataset_dir = config.project_dir+'fake_gt/'
+dataset_dir = config.project_dir+'update_no_background/'
 
 ###########################################################################
 
@@ -21,27 +21,12 @@ for image_id, instance in instances.items():
     image_width = instance['width']
     image_height = instance['height']
 
-    fish_annotations = []
-    sponge_annotations = []
     annotations = []
 
     for annotation in instance['annotations']:
 
-        if annotation['category_id'] == 0:
-            fish_annotations.append(annotation)
-
-        else:
+        if annotation['category_id'] != len(config.categories)-1:
             annotations.append(annotation)
-
-            if annotation['category_id'] == 2:
-                sponge_annotations.append(annotation)
-
-    for fish_annotation in fish_annotations:
-        for sponge_annotation in sponge_annotations:
-            if util.get_bbox1_intersection(fish_annotation['bbox'], sponge_annotation['bbox']) > 0.5:
-                break
-        else:
-            annotations.append(fish_annotation)
 
     instance['annotations'] = annotations
 
