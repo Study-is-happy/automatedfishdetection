864c8d952a8fe51c0efdf2838c057d1d808b796e
Study-is-happy <zhang.zhiyo@husky.neu.edu>
2020-08-03 15:22:24 -0400

    t

--- filter_background.py
+++ filter_background.py
@@ -7,7 +7,7 @@ import util
 
 # TODO: Set the dirs
 
-dataset_dir = config.project_dir+'update/'
+dataset_dir = config.project_dir+'update_no_background/'
 
 ###########################################################################
 
