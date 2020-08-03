7bf46c51124aac6c6821d0e1ff7dde0b7e0f72cb
Study-is-happy <zhang.zhiyo@husky.neu.edu>
2020-08-03 01:12:20 -0400

    t

--- reset_instances.py
+++ reset_instances.py
@@ -3,7 +3,7 @@ import os
 import config
 import util
 
-instances_file_path = config.project_dir+'easy/instances.json'
+instances_file_path = config.project_dir+'update/instances.json'
 
 if os.path.exists(instances_file_path):
     os.remove(instances_file_path)
