2881dafbe3e18c88c5e4ec7ff6176f8d999c3e4b
haha@haha.com <haha@haha.com>
2020-12-04 03:45:57 -0500

    t

--- upload_s3.py
+++ upload_s3.py
@@ -3,20 +3,14 @@ import os
 
 import config
 
-resource = boto3.resource('s3',
-                          aws_access_key_id=config.aws_access_key_id,
-                          aws_secret_access_key=config.aws_secret_access_key)
+s3_bucket = boto3.resource('s3',
+                           aws_access_key_id=config.aws_access_key_id,
+                           aws_secret_access_key=config.aws_secret_access_key).Bucket('datasetocean')
 
-bucket = resource.Bucket('datasetocean')
-bucket.objects.filter(Prefix="zhiyongzhang/predict/").delete()
-
-client = boto3.client('s3',
-                      aws_access_key_id=config.aws_access_key_id,
-                      aws_secret_access_key=config.aws_secret_access_key)
+s3_bucket.objects.filter(Prefix="zhiyongzhang/predict/").delete()
 
 for root, dirs, files in os.walk(config.project_dir+'predict/'):
     for file in files:
         full_file_path = os.path.join(root, file)
-        client.upload_file(full_file_path, 'datasetocean', 'zhiyongzhang/' + os.path.relpath(
+        s3_bucket.upload_file(full_file_path, 'zhiyongzhang/' + os.path.relpath(
             full_file_path, config.project_dir), ExtraArgs={'ACL': 'public-read'})
-        print(file)
