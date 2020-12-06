3c746b91f8d38f500e64da774402c2763a3e1213
auv <zhang.zhiyo@northeastern.edu>
2020-12-06 15:04:18 -0500

    t

--- upload_s3.py
+++ upload_s3.py
@@ -11,17 +11,17 @@ upload_dir = 'images/'
 
 ###########################################################################
 
-s3_dir = 'zhiyongzhang/predict/'+upload_dir
+s3_bucket_dir = 'zhiyongzhang/predict/'+upload_dir
 local_dir = config.project_dir+'predict/'+upload_dir
 
 s3_bucket = boto3.resource('s3',
                            aws_access_key_id=config.aws_access_key_id,
                            aws_secret_access_key=config.aws_secret_access_key).Bucket('datasetocean')
 
-s3_bucket.objects.filter(Prefix=s3_dir).delete()
+s3_bucket.objects.filter(Prefix=s3_bucket_dir).delete()
 
 
 for file_path in os.listdir(local_dir):
 
-    s3_bucket.upload_file(local_dir + file_path, s3_dir +
+    s3_bucket.upload_file(local_dir + file_path, s3_bucket_dir +
                           file_path, ExtraArgs={'ACL': 'public-read'})
